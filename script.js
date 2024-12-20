b65818a9a18dbc09576832b676700189645e1af8
abdilakhatovak <kanykei.abdilakhatova@alatoo.edu.kg>
2024-12-21 00:03:20 +0600

    buttons

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 var display = document.querySelector('.display'); 
 var buttons = document.querySelectorAll('.buttons button'); 
 
-var currentInput = '';
+var currentInput = ''; 
 var result = 0; 
+
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var value = this.textContent;
+
+        if (value === 'C') {
+            
+            currentInput = '';
+            result = 0;
+            display.textContent = '0';
+        } else if (value === '=') {
+            
+            if (currentInput.includes('/0')) {
+               
+                result = 0;
+            } else {
+                try {
+                    result = eval(currentInput); 
+                } catch {
+                    result = 0; 
+                }
+            }
+            display.textContent = result; 
+            currentInput = ''; 
+        } else {
+            
+            currentInput += value;
+            display.textContent = currentInput; 
+        }
+    });
+});
